d70ac1989e05f6d20d3f1c4125c9fbdd5399b1e2
a1dotls <meeae9408@gmail.com>
2023-12-12 23:01:23 +0900

    refactor List 컴포넌트 분리하기

--- React_Practice/react-todo-app/src/components/Lists.js
+++ React_Practice/react-todo-app/src/components/Lists.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import List from "./List";
 
 export default function Lists({ todoData, setTodoData }) {
   const handleEnd = (result) => {
@@ -12,22 +13,6 @@ export default function Lists({ todoData, setTodoData }) {
     const [reorderedItem] = newTodoData.splice(result.source.index, 1);
 
     newTodoData.splice(result.destination.index, 0, reorderedItem);
-    setTodoData(newTodoData);
-  };
-
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-
-      return data;
-    });
-    setTodoData(newTodoData);
-  };
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
     setTodoData(newTodoData);
   };
 
@@ -44,40 +29,16 @@ export default function Lists({ todoData, setTodoData }) {
                   index={index}
                 >
                   {(provided, snapshot) => (
-                    <div
+                    <List
                       key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-                    >
-                      <div className="items-center">
-                        <input
-                          type="checkbox"
-                          defaultChecked={data.completed}
-                          onChange={() => handleCompleteChange(data.id)}
-                        />
-                        {"  "}
-                        <span
-                          className={
-                            data.completed ? "line-through" : undefined
-                          }
-                        >
-                          {data.title}
-                        </span>
-                      </div>
-
-                      <div className="items-center">
-                        <button
-                          className="px-4 py-2 float-right"
-                          onClick={() => handleClick(data.id)}
-                        >
-                          X
-                        </button>
-                      </div>
-                    </div>
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
                   )}
                 </Draggable>
               ))}
