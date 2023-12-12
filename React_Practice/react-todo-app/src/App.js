91cddcf695e40deeda42ae249c5cee1161471d5e
a1dotls <meeae9408@gmail.com>
2023-12-12 23:20:38 +0900

    feat 리스트 모두 지우기 기능 추가

--- React_Practice/react-todo-app/src/App.js
+++ React_Practice/react-todo-app/src/App.js
@@ -7,6 +7,9 @@ export default function App() {
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
 
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -33,6 +36,7 @@ export default function App() {
       <div className="w-full p-6 m-4 bg-white rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
 
         <Lists
