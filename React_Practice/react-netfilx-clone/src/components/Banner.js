9bd0ee981d59e55097c67e593ec92487b58528b0
a1dotls <meeae9408@gmail.com>
2023-12-14 00:33:15 +0900

    feat Styled Component 사용하여 비디오 배너 생성

--- React_Practice/react-netfilx-clone/src/components/Banner.js
+++ React_Practice/react-netfilx-clone/src/components/Banner.js
@@ -2,9 +2,11 @@ import axios from "../api/axios";
 import requests from "../api/requests";
 import React, { useState, useEffect } from "react";
 import "./Banner.css";
+import styled from "styled-components";
 
 export default function Banner() {
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   // Component가 처음 실행될 때 배너 정보를 가지고 있어야 하기 때문에 fetchData()를 call
   useEffect(() => {
@@ -37,26 +39,82 @@ export default function Banner() {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   };
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie.title || movie.name || movie.original_name}
-        </h1>
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button info">More Information</button>
+  // play 버튼 클릭하지 않았을 때 기본 화면
+  if (!isClicked) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
+            <button className="banner__button info">More Information</button>
+          </div>
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 100)}
+          </h1>
         </div>
-        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
-      </div>
-      <div className="banner--fadeBottom"></div>
-    </header>
-  );
+        <div className="banner--fadeBottom"></div>
+      </header>
+    );
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            width="640"
+            height="360"
+            frameborder="0"
+            allow="autoplay;
+            fullscreen"
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
+  }
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
