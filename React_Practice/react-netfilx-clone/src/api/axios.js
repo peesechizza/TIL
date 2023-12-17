6abc2785cf9bc2db6f320f618e2a48c383c93d11
a1dotls <meeae9408@gmail.com>
2023-12-17 20:45:28 +0900

    netflix app

--- React_Practice/react-netfilx-clone/src/api/axios.js
+++ React_Practice/react-netfilx-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "b96c3092d214a4f2e054322908622fa3",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
